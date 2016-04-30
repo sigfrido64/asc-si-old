2971ad68b2406fb21aec073d78a4dd23a2fb01c1
sigfrido64 <pilone@scuolacamerana.it>
2016-04-30 09:00:12 +0200

    Completo prima di rimozione di Aziende Helpers

---
+++ aziende/urls.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from django.conf.urls import url
+from .views import index, get_listanazioni, add_edit, get_listaziende, popola
+from .views import importanazioni, importaprovincie, importaaziende, importacontatti, api_refreshelper
+__author__ = 'Sig'
+
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^add/$', add_edit, name='add'),
+    url(r'^edit/(?P<pk>\w+)$', add_edit, name='edit'),
+    # Sezione della API
+    url(r'^api/get_listanazioni/$', get_listanazioni, name='get_listanazioni'),
+    url(r'^api/2/$', get_listaziende),
+    url(r'^api/3/$', popola),
+    # Importazioni da Assocam
+    url(r'^importa/nazioni/$', importanazioni, name='_i_nazioni'),
+    url(r'^importa/provincie/$', importaprovincie, name='_i_provincie'),
+    url(r'^importa/aziende/$', importaaziende, name='_i_aziende'),
+    url(r'^importa/contatti/$', importacontatti, name='_i_contatti'),
+    url(r'^importa/rh/$', api_refreshelper, name='_i_refresh_helper'),
+]
